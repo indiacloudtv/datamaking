3b81f6e83f9263ccc85f55f6e52bcd8eacd808ed
indiacloudtv <indiacloudtv@gmail.com>
2020-12-16 20:28:45 +0530

    Allowed Hosts Changes

--- datamaking/settings.py
+++ datamaking/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'z2pva__*4eep$#5oz6iz)-l5d+jw5wjh*q1ardi^)mh^5fpa^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
